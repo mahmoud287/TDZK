16d3771bd7a98a4b1f33f5694e7fed1ff862c8a6
Mahmoud Sherif <sherifmahmoud928@gmail.com>
2019-03-22 16:05:30 +0200

    this my template

--- src/main.js
+++ src/main.js
@@ -1,10 +1,38 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import './plugins/ant-design-vue.js'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCoffee);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
 Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
+/* global $ */
+/* global window */
+window.$ = window.jQuery = require('jquery');
+Vue.use({
+  install: function (Vue) {
+    Vue.prototype.$jQuery = require('jquery'); // you'll have this.$jQuery anywhere in your vue project
+  }
+});
+$(function () {
+  $('h1').css('color', 'red')
+});
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
+// ────────────────────────────────────────────────────────────────────────────────
